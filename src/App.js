da27be081368f7c62256e5e7f92e27f0d7b0f2af
nobel <nobelinderpreet91@gmail.com>
2021-01-28 18:48:31 +1100

    Files removed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import FirstSchedular from "./Components/FirstSchedular";
 // import 'antd/dist/antd.css';
 // import 'antd/dist/antd.less';
 import SecondScheduler from "./Components/SecondScheduler";
-import ThirdScheduler from "./Components/ThirdScheduler";
+
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <FirstSchedular/>
+     <SecondScheduler/>
     </div>
   );
 }
